5130f088e81c6368965d90e038acceca5a2a6ee1
Navtej Singh <Nsingh6760@conestogac.on.ca>
2024-06-06 11:43:35 -0400

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit;
+using NUnitDemonstration;
+using NUnit.Framework.Legacy;
+
+namespace TestClass
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+    public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+        {
+
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+            string expected = "The triangle is valid.";
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+            ClassicAssert.AreEqual(expected, actual);
+            
+
+    }
+
+
+        
+
+
+
+    }
+}
+
